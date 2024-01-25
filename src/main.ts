b2aa19515600f795ed3d402a228a454b7085d658
Dave Leaver <danzel@localhost.geek.nz>
2024-01-25 14:24:01 +1300

    No longer MIT, Spaces -> Tabs

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,23 @@ import Phaser from "phaser";
 import HelloScene from "./scenes/HelloScene";
 
 const config: Phaser.Types.Core.GameConfig = {
-    parent: "app",
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scale: {
-        mode: Phaser.Scale.ScaleModes.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: [
-        HelloScene,
-    ]
+	parent: "app",
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	scale: {
+	 mode: Phaser.Scale.ScaleModes.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+	scene: [
+		HelloScene,
+	]
 };
 
 export default new Phaser.Game(config);
